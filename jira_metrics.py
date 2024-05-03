c951ea88764c2f0e72fcb829d5150afc8116e1d9
Thomas Yu <thomas.yu@sagebase.org>
2024-05-02 23:02:35 -0700

    Add in code to get state of ticket at time of sprint closure, and push to snowflake

--- jira_metrics.py
+++ jira_metrics.py
@@ -1,8 +1,11 @@
 import os
+import datetime
+import pytz
 
 from jira import JIRA
 import jira
 import pandas as pd
+import requests
 
 
 def get_issues_per_sprint(jira_client: JIRA, sprint: jira.resources.Sprint) -> pd.DataFrame:
@@ -33,8 +36,10 @@ def get_issues_per_sprint(jira_client: JIRA, sprint: jira.resources.Sprint) -> p
             issue_assignee = None
         # Story points
         issue_story_points = issue_info.raw['fields'].get("customfield_10014")
+        # print(issue_info.raw['fields'].get("customfield_10440"))
         # 'customfield_12105': '6.0',
         # Status of ticket
+        last_status = extract_last_status(sprint.endDate, issue.id)
         issue_status = issue_info.fields.status.name
         issue_summary = issue_info.fields.summary
         issue_desc = issue_info.fields.description
@@ -46,10 +51,11 @@ def get_issues_per_sprint(jira_client: JIRA, sprint: jira.resources.Sprint) -> p
         result.append({
             'sprint_id': sprint.id,
             "issuetype": issue_type_name,
-            "key": issue.id,
-            "summary": issue_summary,
-            "description": issue_desc,
-            'status': issue_status,
+            "id": issue.id,
+            "key": issue_info.key,
+            #"summary": issue_summary,
+            #"description": issue_desc,
+            'status': last_status,
             "assignee": issue_assignee,
             'story_points': issue_story_points,
             "sprint": sprint.name,
@@ -58,6 +64,45 @@ def get_issues_per_sprint(jira_client: JIRA, sprint: jira.resources.Sprint) -> p
         })
     jira_issues_df = pd.DataFrame(result)
     return jira_issues_df
+
+
+def extract_last_status(end_date: str, ticket_id: str) -> str:
+    """Go through changelog and extract the last status
+
+    Args:
+        end_date: End date of a sprint
+        ticket_id: Jira ticket id
+
+    Return:
+        Status"""
+    username = os.environ['JIRA_USERNAME']
+    api_token = os.environ['JIRA_API_TOKEN']
+    base_url = 'https://sagebionetworks.jira.com/'
+    auth = requests.auth.HTTPBasicAuth(username, api_token)
+    timezone = pytz.timezone('UTC')
+
+    # start_datetime = timezone.localize(datetime.datetime.strptime(start_date, "%Y-%m-%d"))
+    end_datetime = timezone.localize(datetime.datetime.strptime(end_date, "%Y-%m-%dT%H:%M:%S.%fZ"))
+
+    # Set up the request headers with authentication
+    headers = {
+        "Accept": "application/json"
+    }
+    url = f"{base_url}/rest/api/3/issue/{ticket_id}/changelog"
+
+    response = requests.get(url, headers=headers, auth=auth)
+
+    if response.status_code == 200:
+        data = response.json()
+        changes_log_df = pd.DataFrame(pd.DataFrame(data)['values'].tolist())
+        changes_log_df['created'] = pd.to_datetime(changes_log_df['created'], utc=True)
+        # Get the latest status prior to the closure of a sprint
+        change_types = changes_log_df[changes_log_df.created <= end_datetime].sort_values('created', ascending=False)['items']
+        for changes in change_types:
+            for change in changes:
+                if change['field'] == "status":
+                    status = change['toString']
+                    return status
 
 
 def main():
@@ -76,10 +121,11 @@ def main():
     all_sprint_info = pd.DataFrame()
     for sprint in all_sprints:
         if sprint.name.startswith("DPE") and "Sprint" not in sprint.name and "12.19.22" not in sprint.name:
-            print(sprint.name)
+            print(sprint.name, sprint.id, sprint.startDate, sprint.endDate)
             df = get_issues_per_sprint(jira_client=jira_client, sprint=sprint)
             all_sprint_info = pd.concat([all_sprint_info, df])
     all_sprint_info.to_csv("dpe_sprint_info.csv", index=False)
+
 
 if __name__ == "__main__":
     main()
---
+++ push_to_snowflake.py
@@ -0,0 +1,32 @@
+from dotenv import dotenv_values
+import snowflake.connector
+from snowflake.connector.pandas_tools import write_pandas
+import synapseclient
+import pandas as pd
+
+
+syn = synapseclient.login()
+
+config = dotenv_values("../.env")
+
+ctx = snowflake.connector.connect(
+    user=config['user'],
+    password=config['password'],
+    account=config['snowflake_account'],
+    database="sage",
+    schema="DPE",
+    role="SYSADMIN",
+    warehouse="compute_xsmall"
+)
+
+cs = ctx.cursor()
+ent_df = pd.read_csv("dpe_sprint_info.csv")
+
+write_pandas(
+    ctx,
+    ent_df,
+    "jira_metrics",
+    auto_create_table=True,
+    overwrite=True,
+    quote_identifiers=False
+)
